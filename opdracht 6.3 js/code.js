2bfe183be37f984aac8235811bde0a7362a42096
dylantcr <9021142@student.zadkine.nl>
2023-10-04 12:01:10 +0200

    6.3 js

--- opdracht 6.3 js/code.js
+++ opdracht 6.3 js/code.js
@@ -1,6 +1,6 @@
 function KleurAanpassen()
 {
     document.getElementById("kop1").style.color = "blue";
-    document.getElementById("kop1").innerHTML = "i have changed";
+    document.getElementById("kop1").innerHTML = "Welkom";
 }
 
