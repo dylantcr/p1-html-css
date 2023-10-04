fe49a57b57a65c4801c90c0e2ff12eb2413263b8
dylantcr <9021142@student.zadkine.nl>
2023-10-04 11:34:52 +0200

    6.1 js

---
+++ opdracht 6.3 js/code.js
@@ -0,0 +1,6 @@
+function KleurAanpassen()
+{
+    document.getElementById("kop1").style.color = "blue";
+    document.getElementById("kop1").innerHTML = "i have changed";
+}
+
---
+++ opdracht 6.4 js/code.js
@@ -0,0 +1,20 @@
+function KleurAanpassen()
+{
+    document.getElementById("kop1").style.color = "blue";
+    document.getElementById("kop1").innerHTML = "i have changed";
+}
+
+function Reset()
+{
+    document.getElementById("kop1").style.color = "black";
+    document.getElementById("kop1").innerHTML = "welkom";
+}
+
+function Calculate(getal1, getal2)
+{
+    antwoord = getal1 + getal2;
+    console.log("Hello world!");
+    console.log(antwoord);
+}
+
+Calculate(5,8);
