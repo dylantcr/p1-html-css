2141c71101b306705ddf4d48ad4881deac100c29
dylantcr <9021142@student.zadkine.nl>
2023-10-04 09:46:31 +0200

    js 6.1

---
+++ opdracht 5.1 js/javascrip.js
@@ -0,0 +1,18 @@
+function changeColor() {
+
+    document.getElementById("id").style.color = "red";
+    
+    }
+    
+    function changeColor() {
+    
+        document.getElementById("id").style.color = "red";
+    
+        }
+    
+    
+    
+    
+        const myBtn = document.querySelector('.my-class');
+        const currentText = myBtn.textContent;
+        myBtn.textContent = currentText + ' en dit komt er bij';
