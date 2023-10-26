8298f52a81b281fae27b615d73658cf1eaf3b506
dylantcr <9021142@student.zadkine.nl>
2023-10-26 10:02:14 +0200

    7.2 js

---
+++ opdracht 7.2 js/app.js
@@ -0,0 +1,13 @@
+let product = 100;
+//let budget = 10;
+let budget = prompt("hoeveel geld heb je?". 0);
+
+let Resultp = document.getElementById("ResultParagraph");
+if (product >= budget) {
+    Resultp.innerHTML= "Helaas, te weinig geld!";
+    Resultp.style.color = "red";
+    }
+else {
+    Resultp.innerHTML = "U heeft genoeg geld!";
+    Resultp.style.color = "green"
+}
